335cc3b4d5a3e32186821d2028bfa4234967eef5
V-Markovskis <vlads.markovskis@gmail.com>
2024-02-02 13:57:16 +0200

    Assets table

--- src/Components/AssetsTable.tsx
+++ src/Components/AssetsTable.tsx
@@ -1,3 +1,44 @@
+import { Table } from 'antd';
+import type { TableColumnsType } from 'antd';
+import { useCrypto } from '../context/crypto-context.tsx';
+
+interface DataType {
+  key: string;
+  name: string | undefined;
+  price: number;
+  amount: number;
+}
+
+const columns: TableColumnsType<DataType> = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    sorter: (a, b) => a.name!.length - b.name!.length,
+    sortDirections: ['descend'],
+  },
+  {
+    title: 'Price, $',
+    dataIndex: 'price',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.price - b.price,
+  },
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.amount - b.amount,
+  },
+];
+
 export default function AssetsTable() {
-  return <div>test</div>;
+  const { assets } = useCrypto();
+
+  const data = assets.map((asset) => ({
+    key: asset.id,
+    name: asset.name,
+    price: asset.price,
+    amount: asset.amount,
+  }));
+
+  return <Table pagination={false} columns={columns} dataSource={data} />;
 }
