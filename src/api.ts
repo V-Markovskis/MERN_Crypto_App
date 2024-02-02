bd7624efa41708f0c52aec57618ca6e0428be837
V-Markovskis <vlads.markovskis@gmail.com>
2024-02-02 16:39:08 +0200

    axios get data

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,32 @@
 import { cryptoAssets, cryptoData } from './data.ts';
-import { CryptoData } from './DataTypes/Crypto/CryptoData.ts';
 import { CryptoAsset } from './DataTypes/Assets/CryptoAsset.ts';
+import axios from 'axios';
 
-export function fetchCrypto(): Promise<CryptoData> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(cryptoData);
-    }, 1);
-  });
+// use hardcoded data in data.ts
+// export function fetchCrypto(): Promise<CryptoData> {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(cryptoData);
+//     }, 1);
+//   });
+// }
+
+export async function fetchCrypto() {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      'X-API-KEY': 'UF7tA8M7HBW36qleNjr+4Xl+z66jFCDdmk+NiZvPxZ4=',
+    },
+  };
+
+  try {
+    const response = await axios.get('https://openapiv1.coinstats.app/coins', options);
+    console.log(response);
+    return response.data;
+  } catch (err) {
+    console.error('Failed to fetch data', err);
+  }
 }
 
 export function fetchAssets(): Promise<CryptoAsset[]> {
