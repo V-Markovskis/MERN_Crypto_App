db7b62c5d227354e73165116b2a1b45606687001
V-Markovskis <vlads.markovskis@gmail.com>
2024-02-11 22:00:06 +0200

    frontend / vercel URI

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -49,7 +49,7 @@ export async function fetchAssets(): Promise<CryptoAsset[]> {
 
 export async function postAsset(asset: CryptoAsset) {
   try {
-    await axios.post('https://mern-crypto-app-backend.vercel.app:3000/assets', asset);
+    await axios.post('https://mern-crypto-app-backend.vercel.app', asset);
   } catch (err) {
     console.error(err);
   }
@@ -57,7 +57,7 @@ export async function postAsset(asset: CryptoAsset) {
 
 export async function deleteAsset(id: string) {
   try {
-    await axios.delete(`https://mern-crypto-app-backend.vercel.app:3000/assets/${id}`);
+    await axios.delete(`https://mern-crypto-app-backend.vercel.app/${id}`);
   } catch (err) {
     console.error(err);
   }
